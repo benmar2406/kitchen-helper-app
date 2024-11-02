2f9eb2fc76c04bb304e788f6f20772e9cf3cbc3b
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2024-11-02 09:00:50 +0100

    Update useGetSelectedRecipeDetails.jsx

--- client/src/hooks/useGetSelectedRecipeDetails.jsx
+++ client/src/hooks/useGetSelectedRecipeDetails.jsx
@@ -1,6 +1,4 @@
-import React, { useContext, useState } from 'react';
 import { useQuery } from 'react-query';
-import { RecipesContext } from '../context/RecipesContext';
 
 const useGetSelectedRecipe = (id) => {
   
