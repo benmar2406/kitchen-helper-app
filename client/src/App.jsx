f090e2db0f74913eaebd54039df43de5d26db3a8
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-07-30 13:25:50 +0200

    upd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-  import React from 'react';
   import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
   import { FormProvider } from './context/FormContext';
   import { RecipesProvider } from './context/RecipesContext';
